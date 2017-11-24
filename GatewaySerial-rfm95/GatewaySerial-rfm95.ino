fd31451b31c72509f04d6dd89db3edefa2871e3b
Lee Nelson <lnelson@nelnet.org>
2017-11-23 17:28:41 -0800

    First Version to register
    Radio communication fails

--- GatewaySerial-rfm95/GatewaySerial-rfm95.ino
+++ GatewaySerial-rfm95/GatewaySerial-rfm95.ino
@@ -37,34 +37,38 @@
 */
 
 // Enable debug prints to serial monitor
-#define MY_DEBUG
-
+//#define MY_DEBUG
+//Want DEBUG output to Serial (USB Port)
+//#define MY_DEBUG_HWSERIAL Serial  // Tried 'Serial' and 'Serial0'
 
 // Enable and select radio type attached
 //#define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
-
-
 // Set LOW transmit power level as default, if you have an amplified NRF-module and
 // power your radio separately with a good regulator you can turn up PA level.
 //#define MY_RF24_PA_LEVEL RF24_PA_LOW
 
+//#define   MY_RFM95_FREQUENCY RFM95_915MHZ
+//#define MY_RFM95_MODEM_CONFIGRUATION RFM95_BW31_25CR48SF512
+
 #define   RFM95_IRQ_PIN  6
 #define   RFM95_RST_PIN  9
-#define   RFM95_SPI_CS 10
+#define   RFM95_SPI_CS   10
 #define   MY_RADIO_RFM95
-#define   MY_DEBUG_VERBOSE_RFM95
+//#define   MY_DEBUG_VERBOSE_RFM95
 
 // Enable serial gateway
 #define MY_GATEWAY_SERIAL
+//Want Gateway <-> Controller communication over Serial1
+#define MY_SERIALDEVICE Serial1
 
 // Define a lower baud rate for Arduino's running on 8 MHz (Arduino Pro Mini 3.3V & SenseBender)
-#if F_CPU == 8000000L
+//#if F_CPU == 8000000L
 #define MY_BAUD_RATE 38400
-#endif
+//#endif
 
 // Enable inclusion mode
-#define MY_INCLUSION_MODE_FEATURE
+//#define MY_INCLUSION_MODE_FEATURE
 // Enable Inclusion mode button on gateway
 //#define MY_INCLUSION_BUTTON_FEATURE
 
@@ -72,12 +76,12 @@
 //#define MY_INCLUSION_BUTTON_EXTERNAL_PULLUP
 
 // Set inclusion mode duration (in seconds)
-#define MY_INCLUSION_MODE_DURATION 60
+//#define MY_INCLUSION_MODE_DURATION 60
 // Digital pin used for inclusion mode button
 //#define MY_INCLUSION_MODE_BUTTON_PIN  3
 
 // Set blinking period
-#define MY_DEFAULT_LED_BLINK_PERIOD 300
+//#define MY_DEFAULT_LED_BLINK_PERIOD 300
 
 // Inverses the behavior of leds
 //#define MY_WITH_LEDS_BLINKING_INVERSE
@@ -86,21 +90,71 @@
 // Uncomment to override default HW configurations
 //#define MY_DEFAULT_ERR_LED_PIN 4  // Error led pin
 //#define MY_DEFAULT_RX_LED_PIN  6  // Receive led pin
-//#define MY_DEFAULT_TX_LED_PIN  5  // the PCB, on board LED
+//#define MY_DEFAULT_TX_LED_PIN  13  // the PCB, on board LED
 
 #include <MySensors.h>
+#define LED_ONBOARD 13
+#define RELAY_1  11  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
+#define NUMBER_OF_RELAYS 1 // Total number of attached relays
+#define RELAY_ON 1  // GPIO value to write to turn on attached relay
+#define RELAY_OFF 0 // GPIO value to write to turn off attached relay
+
+#define CHILD_ID 1
+bool state = false;
+bool initialValueSent = false;
+
+MyMessage msg(CHILD_ID, V_STATUS);
+
+
 
 void setup()
 {
 	// Setup locally attached sensors
+  //
+  //Turn On on-board LED as power indicator
+  pinMode(LED_ONBOARD, OUTPUT);
 }
 
 void presentation()
 {
+  sendSketchInfo("SerialGW+Relay", "0.2");
 	// Present locally attached sensors
+  for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
+    present(sensor, S_BINARY);
+    // Then set relay pins in output mode
+    pinMode(pin, OUTPUT);
+    // Set relay to last known state (using eeprom storage)
+    digitalWrite(pin, loadState(sensor)?RELAY_ON:RELAY_OFF);
+  }
 }
 
 void loop()
 {
+  if (!initialValueSent) {
+    Serial.println("Sending initial value");
+    send(msg.set(state?RELAY_ON:RELAY_OFF));
+    Serial.println("Requesting initial value from controller");
+    request(CHILD_ID, V_STATUS);
+    wait(2000, C_SET, V_STATUS);
+  }
 	// Send locally attached sensor data here
+  //Turn On on-board LED as power indicator
+  digitalWrite(LED_ONBOARD, HIGH);
 }
+
+void receive(const MyMessage &message)
+{
+  // We only expect one type of message from controller. But we better check anyway.
+  if (message.type==V_STATUS) {
+    // Change relay state
+    digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
+    // Store state in eeprom
+    saveState(message.sensor, message.getBool());
+    // Write some debug info
+    Serial.print("Incoming change for sensor:");
+    Serial.print(message.sensor);
+    Serial.print(", New status: ");
+    Serial.println(message.getBool());
+  }
+}
+
