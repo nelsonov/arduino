deda024125aa9d516c381b2e88917a42eec146fe
Lee Nelson <lnelson@nelnet.org>
2017-12-04 19:30:20 -0800

    Add Serail Echo scipts

---
+++ SerialEchoReply/SerialEchoReply.ino
@@ -0,0 +1,45 @@
+#include <AltSoftSerial.h>
+
+
+String str;
+int rxcount;
+
+AltSoftSerial altSerial;
+
+#define RS485_DE_PIN 7
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  altSerial.begin(9600);
+#ifdef RS485_DE_PIN
+  pinMode(RS485_DE_PIN, OUTPUT);
+  digitalWrite(RS485_DE_PIN, LOW);
+#endif
+  rxcount=-1;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(altSerial.available() > 0)
+  {
+      str = altSerial.readStringUntil('\t');
+      Serial.print(str);
+      rxcount = altSerial.parseInt();
+      str = altSerial.readStringUntil('\n');
+      Serial.print(" ");
+      Serial.println(rxcount);
+#ifdef RS485_DE_PIN
+      digitalWrite(RS485_DE_PIN, HIGH);
+      delay(10);
+#endif    
+      altSerial.print("Echo Reply:   ");
+      altSerial.print(rxcount);
+#ifdef RS485_DE_PIN
+      altSerial.flush();
+      delay(10);
+      digitalWrite(RS485_DE_PIN, LOW);
+#endif    
+  }
+
+}
---
+++ SerialEchoRequest/SerialEchoRequest.ino
@@ -0,0 +1,48 @@
+#include <AltSoftSerial.h>
+
+
+String str;
+int txcount;
+AltSoftSerial altSerial;
+
+#define RS485_DE_PIN 7
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  altSerial.begin(9600);
+#ifdef RS485_DE_PIN
+  pinMode(RS485_DE_PIN, OUTPUT);
+  digitalWrite(RS485_DE_PIN, LOW);
+#endif
+  txcount=0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("TX Echo Request: ");
+  Serial.println(txcount);
+#ifdef RS485_DE_PIN
+  digitalWrite(RS485_DE_PIN, HIGH);
+  delay(10);
+#endif    
+  altSerial.print("Echo Request\t");
+  altSerial.println(txcount);
+#ifdef RS485_DE_PIN
+  altSerial.flush();
+  delay(10);
+  digitalWrite(RS485_DE_PIN, LOW);
+#endif  
+  delay(1000);
+  if(altSerial.available() > 0)
+  {
+    str = altSerial.readString();
+    Serial.print("RX ");
+    Serial.println(str);
+  } else {
+    Serial.print("TO:              ");
+    Serial.println(txcount);
+  }
+  txcount++;
+}
+
