076df5b2c6a85e56f3e2c75dec8f36c2b132d3b8
Lee Nelson <lnelson@nelnet.org>
2017-12-07 00:26:12 -0800

    Add light-sensor-test

---
+++ light-sensor-test/light-sensor-test.ino
@@ -0,0 +1,22 @@
+#define SAMPLE 20                    //this is how many SAMPLEs the device takes per reading
+#define ANALOG_PIN A0
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop(void)
+{
+  int16_t adc0 = 0;
+  long sum = 0;
+  int16_t reportval = 0;
+  int i;
+  for (i=0; i < SAMPLE; i++) {
+    adc0 = analogRead(ANALOG_PIN);
+    sum += adc0;
+  }
+  reportval = 1024 - (sum / SAMPLE) ;
+  Serial.println(reportval);
+  delay(1000);
+}
+
