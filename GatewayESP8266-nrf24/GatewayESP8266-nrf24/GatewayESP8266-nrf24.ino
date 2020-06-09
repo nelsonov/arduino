e791fe9dfeeb386ea5136855a324b68a57544083
Lee Nelson <lnelson@nelnet.org>
2020-06-09 00:54:43 -0700

    Updated from MySensors/examples and add structure for arduino-cli

--- GatewayESP8266-nrf24/GatewayESP8266-nrf24/GatewayESP8266-nrf24.ino
+++ GatewayESP8266-nrf24/GatewayESP8266-nrf24/GatewayESP8266-nrf24.ino
@@ -1,4 +1,4 @@
-/**
+*
  * The MySensors Arduino library handles the wireless radio link and protocol
  * between your home built sensors/actuators and HA controller of choice.
  * The sensors forms a self healing radio network with optional repeaters. Each
@@ -6,8 +6,8 @@
  * network topology allowing messages to be routed to nodes.
  *
  * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2015 Sensnology AB
- * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ * Copyright (C) 2013-2019 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/MySensors/graphs/contributors
  *
  * Documentation: http://www.mysensors.org
  * Support Forum: http://forum.mysensors.org
@@ -19,8 +19,7 @@
  *******************************
  *
  * REVISION HISTORY
- * 
- * Version 1.0 - Henrik EKblad
+ * Version 1.0 - Henrik Ekblad
  * Contribution by a-lurker and Anticimex,
  * Contribution by Norbert Truchsess <norbert.truchsess@t-online.de>
  * Contribution by Ivo Pullens (ESP8266 support)
@@ -31,41 +30,27 @@
  *
  * VERA CONFIGURATION:
  * Enter "ip-number:port" in the ip-field of the Arduino GW device. This will temporarily override any serial configuration for the Vera plugin.
- * E.g. If you want to use the defualt values in this sketch enter: 192.168.178.66:5003
+ * E.g. If you want to use the default values in this sketch enter: 192.168.178.66:5003
  *
  * LED purposes:
  * - To use the feature, uncomment any of the MY_DEFAULT_xx_LED_PINs in your sketch, only the LEDs that is defined is used.
- * - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
+ * - RX (green) - blink fast on radio message received. In inclusion mode will blink fast only on presentation received
  * - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
- * - ERR (red) - fast blink on error during transmission error or recieve crc error
+ * - ERR (red) - fast blink on error during transmission error or receive crc error
  *
- * See http://www.mysensors.org/build/esp8266_gateway for wiring instructions.
- * nRF24L01+  ESP8266
- * VCC        VCC
- * CE         GPIO4
- * CSN/CS     GPIO15
- * SCK        GPIO14
- * MISO       GPIO12
- * MOSI       GPIO13
- * GND        GND
+ * See https://www.mysensors.org/build/connect_radio for wiring instructions.
  *
- * Not all ESP8266 modules have all pins available on their external interface.
- * This code has been tested on an ESP-12 module.
- * The ESP8266 requires a certain pin configuration to download code, and another one to run code:
- * - Connect REST (reset) via 10K pullup resistor to VCC, and via switch to GND ('reset switch')
- * - Connect GPIO15 via 10K pulldown resistor to GND
- * - Connect CH_PD via 10K resistor to VCC
- * - Connect GPIO2 via 10K resistor to VCC
- * - Connect GPIO0 via 10K resistor to VCC, and via switch to GND ('bootload switch')
+ * If you are using a "barebone" ESP8266, see
+ * https://www.mysensors.org/build/esp8266_gateway#wiring-for-barebone-esp8266
  *
-  * Inclusion mode button:
- * - Connect GPIO5 via switch to GND ('inclusion switch')
+ * Inclusion mode button:
+ * - Connect GPIO5 (=D1) via switch to GND ('inclusion switch')
  *
  * Hardware SHA204 signing is currently not supported!
  *
  * Make sure to fill in your ssid and WiFi password below for ssid & pass.
  */
-
+#include "secrets.h"
 
 // Enable debug prints to serial monitor
 #define MY_DEBUG
@@ -74,27 +59,26 @@
 #define MY_BAUD_RATE 9600
 
 // Enables and select radio type (if attached)
-#define MY_RADIO_NRF24
+#define MY_RADIO_RF24
 //#define MY_RADIO_RFM69
-//#define MY_RF24_CE_PIN 5  //  Board pin 16  IO 1
-//#define MY_RF24_CS_PIN 4  //  Board pin 15  IO 3
+//#define MY_RADIO_RFM95
 
 #define MY_GATEWAY_ESP8266
 
-#define MY_WIFI_SSID "nelnet"
-#define MY_WIFI_PASSWORD "56seven8"
+//#define MY_WIFI_SSID MYSSID // included from secrets.h
+//#define MY_WIFI_PASSWORD MYWIFIPASSWORD // included from secrets.h
 
 // Enable UDP communication
-//#define MY_USE_UDP
+//#define MY_USE_UDP  // If using UDP you need to set MY_CONTROLLER_IP_ADDRESS or MY_CONTROLLER_URL_ADDRESS below
 
 // Set the hostname for the WiFi Client. This is the hostname
-// it will pass to the DHCP server if not static
-#define MY_HOSTNAME "sensor-gateway"
+// it will pass to the DHCP server if not static.
+#define MY_HOSTNAME "ESP8266_GW"
 
 // Enable MY_IP_ADDRESS here if you want a static ip address (no DHCP)
 //#define MY_IP_ADDRESS 192,168,178,87
 
-// If using static ip you need to define Gateway and Subnet address as well
+// If using static ip you can define Gateway and Subnet address as well
 //#define MY_IP_GATEWAY_ADDRESS 192,168,178,1
 //#define MY_IP_SUBNET_ADDRESS 255,255,255,0
 
@@ -107,20 +91,20 @@
 // Controller ip address. Enables client mode (default is "server" mode).
 // Also enable this if MY_USE_UDP is used and you want sensor data sent somewhere.
 //#define MY_CONTROLLER_IP_ADDRESS 192, 168, 178, 68
+//#define MY_CONTROLLER_URL_ADDRESS "my.controller.org"
 
 // Enable inclusion mode
 //#define MY_INCLUSION_MODE_FEATURE
 
 // Enable Inclusion mode button on gateway
-// #define MY_INCLUSION_BUTTON_FEATURE
+//#define MY_INCLUSION_BUTTON_FEATURE
 // Set inclusion mode duration (in seconds)
 //#define MY_INCLUSION_MODE_DURATION 60
 // Digital pin used for inclusion mode button
-//#define MY_INCLUSION_MODE_BUTTON_PIN  3
-
+//#define MY_INCLUSION_MODE_BUTTON_PIN D1
 
 // Set blinking period
-// #define MY_DEFAULT_LED_BLINK_PERIOD 300
+//#define MY_DEFAULT_LED_BLINK_PERIOD 300
 
 // Flash leds on rx/tx/err
 // Led pins used if blinking feature is enabled above
@@ -128,34 +112,21 @@
 //#define MY_DEFAULT_RX_LED_PIN  16  // Receive led pin
 //#define MY_DEFAULT_TX_LED_PIN  16  // the PCB, on board LED
 
-#if defined(MY_USE_UDP)
-#include <WiFiUdp.h>
-#endif
-
-#include <ESP8266WiFi.h>
-#include <SPI.h>
 #include <MySensors.h>
+#include "secrets.h"
 
 void setup()
 {
-
+	// Setup locally attached sensors
 }
 
 void presentation()
 {
-  sendSketchInfo("GatewayESP8266-nrf24", "0.1");
+	// Present locally attached sensors here
 }
-
 
 void loop()
 {
-
-}
-
-void receive(const MyMessage &message) {
-  // We only expect one type of message from controller. But we better check anyway.
-  if (message.isAck()) {
-     Serial.println("This is an ack from gateway");
-  }
+	// Send locally attached sensors data here
 }
 
